1486f18ba3daf759648043419a909965e709e54c
Marcello Nesca <umnescam@myumanitoba.ca>
2023-09-26 13:56:13 -0600

    Update streamlit_app.py
    starting again Sept 26

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,12 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
