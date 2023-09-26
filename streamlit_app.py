e964ffdc7af16adc9b64ed46c89504c3955803bc
Marcello Nesca <umnescam@myumanitoba.ca>
2023-09-26 14:10:13 -0600

    Update streamlit_app.py
    moved streamlit stop again

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,12 +49,12 @@ def get_fruit_load_list():
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
-streamlit.stop()
-
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
