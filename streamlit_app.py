7ae7797cb6a0334e61154c7fba0c041f77d26fda
Marcello Nesca <umnescam@myumanitoba.ca>
2023-09-26 14:30:44 -0600

    Update streamlit_app.py
    trying steamlit stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,12 +59,15 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding ", + new_fruit
 
-streamlit.stop()
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+
+streamlit.stop()
+  
   streamlit.text(back_from_function)
 
 
